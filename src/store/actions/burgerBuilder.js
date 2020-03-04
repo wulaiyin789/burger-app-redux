a08e1464dcb4716ad154f18d46a994a0daf8a826
Peter Wu <53519949+wulaiyin789@users.noreply.github.com>
2020-03-04 14:35:03 +0800

    Files upload

---
+++ src/store/actions/burgerBuilder.js
@@ -0,0 +1,42 @@
+import axios from '../../axios-orders';
+
+import * as actionTypes from './actionTypes';
+
+export const addIngredient = (name) => {
+    return {
+        type: actionTypes.ADD_INGREDIENT,
+        ingredientName: name
+    };
+};
+
+export const removeIngredient = (name) => {
+    return {
+        type: actionTypes.REMOVE_INGREDIENT,
+        ingredientName: name
+    };
+};
+
+export const setIngredients = (ingredients) => {
+    return {
+        type: actionTypes.SET_INGREDIENT,
+        ingredients: ingredients
+    };
+};
+
+export const fetchIngredientsFailed = () => {
+    return {
+        type: actionTypes.FETCH_INGREDIENTS_FAILED
+    };
+};
+
+export const initIngredients = () => {
+    return dispatch => {
+        axios.get( 'https://react-my-burger-d4f53.firebaseio.com/ingredients.json' )
+        .then( response => {
+            dispatch(setIngredients(response.data));
+        } )
+        .catch( error => {
+            dispatch(fetchIngredientsFailed());
+        } );
+    };
+};
